a4ed8ae27b3b9f8896533c6e9c4959e3d40418cb
daisuke <daisukeejiri@gmail.com>
2022-05-03 14:36:00 +0900

    update

--- script/R/sample.R
+++ script/R/sample.R
@@ -3,6 +3,7 @@
 #######################################
 library(gapminder)
 library(ggplot2)
+library(ggrepel)
 
 #######################################
 # plot
@@ -27,3 +28,18 @@ p <- ggplot2::ggplot(data = gapminder,
 #p + geom_point() + geom_smooth(method = "lm")
 p + geom_point() + geom_smooth(method = "gam") + scale_x_log10()
 
+#######################################
+# plot3
+#######################################
+p <- ggplot2::ggplot(data = gapminder,
+                     mapping = aes(x = year,y = gdpPercap))
+p + geom_line(color = "gray70",
+              mapping = aes(group = country)) +
+  geom_smooth(size = 1.1,
+              method = "loess",
+              se = FALSE) +
+  scale_y_log10(labels = scales::dollar) +
+  facet_wrap(~ continent,ncol = 5) +
+  labs(x = "year",
+       y = "log GDP per capita",
+       title = "log GDP per capita on Five Continents")
